352221037bb3126875a7898b82df1358c9ec77ba
vishnu7191 <vvemula@vfirst.com>
2025-01-30 16:00:46 +0530

    first commit

---
+++ src/Form.jsx
@@ -0,0 +1,94 @@
+import { Button, Container, createTheme, TextField, ThemeProvider, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer, Paper } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+const Form = () => {
+
+    const [deposite,setDeposite]=useState("");
+    const [withdraw,setWithdraw]=useState("");
+    const [mobile,setMobile]=useState("");
+
+  const data=useSelector((state)=>{
+         return state
+  })
+
+ let dispatch=useDispatch()
+
+ const handleDeposite=()=>{
+    dispatch({type:"diposite",payload:deposite})
+    setDeposite("")
+ }
+  const handleWithdraw=()=>{
+    dispatch({type:"withdraw",payload:withdraw})
+    setWithdraw("")
+  }
+  const handleMobile=()=>{
+    dispatch({type:"mobileNumberUpdate",payload:mobile})
+    setMobile("")
+  }
+
+
+    const theme=createTheme({
+        palette: {
+            primary: {
+              main: '#1976d2',
+            },
+            secondary: {
+              main: '#dc004e',
+            },
+            danger:{
+                main:"#f70505"
+            }
+          },
+    })
+
+
+
+
+  return (
+    <div>
+
+    <ThemeProvider theme={theme}>
+    <Container>
+    <Typography variant='h4' color='primary' fontWeight={"bold"} gutterBottom> 
+        Form Using Redux
+    </Typography>
+    <TextField label="Deposite" style={{width:'30%'}} value={deposite} onChange={(e)=>{setDeposite(e.target.value)}}></TextField>  <span> </span>
+    <Button variant='contained' onClick={handleDeposite}>Deposite</Button> <br /><br />
+    <TextField label="Withdraw" style={{width:'30%'}} value={withdraw} onChange={(e)=>{setWithdraw(e.target.value)}}></TextField>  <span> </span>
+    <Button variant='contained' onClick={handleWithdraw} color='danger' style={{color:"white"}}>Withdraw</Button><br /><br />
+    <TextField label="Mobile" style={{width:'30%'}} value={mobile} onChange={(e)=>{setMobile(e.target.value)}}></TextField>  <span> </span>
+    <Button variant='contained' onClick={handleMobile} >Mobile Update</Button>
+    <TableContainer component={Paper} style={{ maxWidth: 500, marginTop: 20 }}>
+        <Typography variant='h4' color='primary' fontWeight={"bold"} gutterBottom>Account Details</Typography>
+      <Table>
+        {/* Table Header */}
+        <TableHead style={{backgroundColor:'primary'}}>
+          <TableRow>
+            <TableCell style={{ color: '#1976d2', fontWeight: 'bold' }}>Amount</TableCell>
+            <TableCell style={{ color: '#1976d2', fontWeight: 'bold' }}>FullName</TableCell>
+            <TableCell style={{ color: '#1976d2', fontWeight: 'bold' }}>Mobile</TableCell>
+          </TableRow>
+        </TableHead>
+
+        {/* Table Body */}
+        <TableBody>
+            <TableRow>
+              <TableCell>{data.balance}</TableCell>
+              <TableCell>{data.fullName}</TableCell>
+              <TableCell>{data.mobileNumber}</TableCell>
+            </TableRow>
+         
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Container>
+    </ThemeProvider>
+
+    </div>
+  )
+}
+
+export default Form
---
+++ src/store.js
@@ -0,0 +1,47 @@
+import { createStore } from "redux"
+
+const initialState={
+    balance:0,
+    fullName:"vishnu",
+    mobileNumber:6305317447,
+}
+
+
+function accountReducer(state=initialState,action){
+
+        switch(action.type){
+             case "diposite":{
+                return {...state,balance:state.balance+ +action.payload}
+            }
+        
+            case "withdraw":{
+                return {...state,balance:state.balance-action.payload}
+            }
+           case "mobileNumberUpdate":{
+                return {...state,mobileNumber:action.payload}
+            }
+            default :{
+                return state
+            }
+        }
+}
+
+
+const store=createStore(accountReducer)
+store.dispatch({type:"diposite",payload:1000})
+console.log(store);
+console.log(store.getState());
+store.dispatch({type:"withdraw",payload:500})
+console.log(store.getState());
+store.dispatch({type:"mobileNumberUpdate",payload:657899})
+console.log(store.getState());
+
+
+export default store
+
+
+
+
+
+
+
